90e0b519073454d508100661d2d85d263fd16429
sme777 <samson.petrosyan8@gmail.com>
2021-06-12 19:29:19 -0700

    feat add sign out option for a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
-    
+    helper_method :signout
+
     def current_user
         return unless session[:user_id]
         @current_user ||= User.find(session[:user_id])
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
 
     def logged_in?
         !!session[:user_id]
+    end
+
+    def signout
+        session[:user_id] = nil
+        redirect_to '/'
     end
 end
